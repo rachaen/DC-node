ea56ab531496f0adb42f101f607bbf2a7aaebff0
rachaen <rachaenlee@gmail.com>
2022-02-24 16:17:22 +0900

    리팩토링-MVC 비동기적용

--- dwitter/server/controller/tweet.js
+++ dwitter/server/controller/tweet.js
@@ -1,14 +1,14 @@
 import * as tweetRepository from '../data/tweet.js';
 
-export function getTweets(req, res) {
+export async function getTweets(req, res) {
   const username = req.query.username;
-  const data = username
+  const data = await (username
     ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll();
+    : tweetRepository.getAll());
   res.status(200).json(data);
 }
 
-export function getTweet(req, res, next) {
+export async function getTweet(req, res, next) {
   const id = req.params.id;
   const tweet = tweetRepository.getById(id);
   if (tweet) {
@@ -18,14 +18,14 @@ export function getTweet(req, res, next) {
   }
 }
 
-export function createTweet(req, res) {
+export async function createTweet(req, res) {
   // 바디를 받아와야 한다.
   const { text, name, username } = req.body;
   const tweet = tweetRepository.create(text, name, username);
   res.status(201).json(tweet); // 사용자에게 추가한 데이터를 보내주기
 }
 
-export function updateTweet(req, res, next) {
+export async function updateTweet(req, res, next) {
   const id = req.params.id;
   const text = req.body.text;
   const tweet = tweetRepository.update(id, text);
@@ -36,8 +36,8 @@ export function updateTweet(req, res, next) {
   }
 }
 
-export function deleteTweet(req, res, next) {
+export async function deleteTweet(req, res, next) {
   const id = req.params.id;
-  tweetRepository.remove(id);
+  await tweetRepository.remove(id);
   res.sendStatus(204); // 정상적으로 삭제했고 리소스에 대한 컨텐츠는 없다!
 }
